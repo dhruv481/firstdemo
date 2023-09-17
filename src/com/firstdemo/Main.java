6fd7130f52fbc27ff69ec5237b0123acc610a7e3
dhruv481 <dguptadg01@gmail.com>
2023-09-18 00:49:58 +0530

    Java Variables

--- src/com/firstdemo/Main.java
+++ src/com/firstdemo/Main.java
@@ -4,6 +4,12 @@ package com.firstdemo;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello Dhruv!");
+            //System.out.println("Hello Dhruv!");
+        String fname = "Dhruv";
+        int age = 22;
+        String lname = "Gupta";
+        String name = fname + " " + lname;
+        System.out.println(name);
+        System.out.println("age = " + age);
         }
     }
