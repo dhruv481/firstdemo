114f792cec10296402a395aa897b4f56fbff5b03
dhruv481 <dguptadg01@gmail.com>
2023-09-19 21:01:39 +0530

    Update README.md

--- README.md
+++ README.md
@@ -16,4 +16,30 @@ String<br>
 Array<br>
 Class<br>
 <b>Always initialise non-primitive data types with new keyword</b>
+<br><br>
+<h3>Strings in Java</h3><br>
+        
+        //Initialization
+        String fname = "Dhruv";
+        String lname = "Gupta";
+
+        //Concatenation
+        String name = fname + " " + lname;
+        System.out.println(name);
+
+        //charAt (starts from index 0)
+        System.out.println(name.charAt(3));
+
+        //Length
+        System.out.println(name.length());
+
+        //replace (works separately for lowercase and uppercase)
+        String demo =  "Aman";
+        String demonew = demo.replace('a','b');
+        System.out.println("old = " + demo + "     and new = " + demonew);
+
+        //Sub-string (first index is taken into substring and last index is not taken)
+        System.out.println(name.substring(6,11));
+
 <br>
+
--- src/com/firstdemo/Main.java
+++ src/com/firstdemo/Main.java
@@ -9,28 +9,7 @@ public class Main {
         int age = 22;       //initialization
         System.out.println("age = " + age);     //Output in Java
 
-        //Strings
-        //initialization
-        String fname = "Dhruv";
-        String lname = "Gupta";
 
-        //Concatenation
-        String name = fname + " " + lname;
-        System.out.println(name);
-
-        //charAt (starts from index 0)
-        System.out.println(name.charAt(3));
-
-        //Length
-        System.out.println(name.length());
-
-        //replace (works separately for lowercase and uppercase)
-        String demo =  "Aman";
-        String demonew = demo.replace('a','b');
-        System.out.println("old = " + demo + "     and new = " + demonew);
-
-        //Sub-string (first index is taken into substring and last index is not taken)
-        System.out.println(name.substring(6,11));
 
         }
     }
