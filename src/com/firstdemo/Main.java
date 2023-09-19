16dff5e6181ff3d98f260e075f123712f4067579
dhruv481 <dguptadg01@gmail.com>
2023-09-19 20:55:50 +0530

    Strings in Java

--- src/com/firstdemo/Main.java
+++ src/com/firstdemo/Main.java
@@ -4,12 +4,33 @@ package com.firstdemo;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-            //System.out.println("Hello Dhruv!");
+        //System.out.println("Hello Dhruv!");  first program
+
+        int age = 22;       //initialization
+        System.out.println("age = " + age);     //Output in Java
+
+        //Strings
+        //initialization
         String fname = "Dhruv";
-        int age = 22;
         String lname = "Gupta";
+
+        //Concatenation
         String name = fname + " " + lname;
         System.out.println(name);
-        System.out.println("age = " + age);
+
+        //charAt (starts from index 0)
+        System.out.println(name.charAt(3));
+
+        //Length
+        System.out.println(name.length());
+
+        //replace (works separately for lowercase and uppercase)
+        String demo =  "Aman";
+        String demonew = demo.replace('a','b');
+        System.out.println("old = " + demo + "     and new = " + demonew);
+
+        //Sub-string (first index is taken into substring and last index is not taken)
+        System.out.println(name.substring(6,11));
+
         }
     }
