db258c8bde0003959b166c07b17e4a1ba08e36c7
dhruv481 <dguptadg01@gmail.com>
2023-09-21 00:36:07 +0530

    Take input in java

--- src/com/firstdemo/Main.java
+++ src/com/firstdemo/Main.java
@@ -1,11 +1,21 @@
 package com.firstdemo;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        //input in java
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.next();
+        Scanner strc = new Scanner(System.in);
+        System.out.println("Enter anything about you: ");
+        String str = strc.nextLine();
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("Name is " + name + ". Age is " + age + ". " + str + ".");
 
         }
     }
