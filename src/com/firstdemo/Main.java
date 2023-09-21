365eba6a0e6ff67c53443358cf64a7ac1b739b83
dhruv481 <dguptadg01@gmail.com>
2023-09-22 00:31:25 +0530

    Loops in JAVA

--- src/com/firstdemo/Main.java
+++ src/com/firstdemo/Main.java
@@ -5,16 +5,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //For Loop
+        //print 1-100
+        for (int i = 1;i<=100; i++){
+            System.out.println(i);
+        }
+
+        //print 100-1
+        for (int j = 100; j>=1; j--){
+            System.out.println(j);
+        }
+
+        //While Loop
+        //final int k = 100;    with final keyword value becomes constant and cannot be changed further.
+        int k = 100;
+        while (k>=1){
+            System.out.println(k);
+            k=k-1;
+        }
+
+        //Do While
+        int l = 100;
+        do {
+            System.out.println(l);
+            l = l - 1;
+        }while (l>=1);
+
         //input in java
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = sc.next();
-        Scanner strc = new Scanner(System.in);
-        System.out.println("Enter anything about you: ");
-        String str = strc.nextLine();
-        System.out.println("Enter your age:");
-        int age = sc.nextInt();
-        System.out.println("Name is " + name + ". Age is " + age + ". " + str + ".");
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter your name:");
+        //String name = sc.next();
+        //Scanner strc = new Scanner(System.in);
+        //System.out.println("Enter anything about you: ");
+        //String str = strc.nextLine();
+        //System.out.println("Enter your age:");
+        //int age = sc.nextInt();
+        //System.out.println("Name is " + name + ". Age is " + age + ". " + str + ".");
 
         }
     }
