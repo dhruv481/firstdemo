f7e5f5d9cf8ef835fc1da14a8298c4e64d536dc9
dhruv481 <dguptadg01@gmail.com>
2023-09-22 16:02:48 +0530

    functions in java

--- src/com/firstdemo/Main.java
+++ src/com/firstdemo/Main.java
@@ -3,7 +3,13 @@ package com.firstdemo;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void printJava(){
+        System.out.println("Hello JAVA.");
+    }
+    public static void main(String[] args) {
+        printJava();
+        printJava();
+        printJava();
         }
     }
