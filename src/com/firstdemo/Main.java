2e2cbfa48e35518bd580dec704decf9d2c9373ac
dhruv481 <dguptadg01@gmail.com>
2023-09-22 16:17:09 +0530

    SUM of two numbers

--- src/com/firstdemo/Main.java
+++ src/com/firstdemo/Main.java
@@ -4,12 +4,23 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int printSum(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
     public static void printJava(){
         System.out.println("Hello JAVA.");
     }
     public static void main(String[] args) {
-        printJava();
-        printJava();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a :");
+        int num1 = sc.nextInt();
+        System.out.print("Enter b:");
+        int num2 = sc.nextInt();
+        System.out.println("The Sum of a and b is : " + printSum(num1, num2));
+
         printJava();
         }
     }
